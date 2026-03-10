58b2b892ac3be164f00712d51b75953ca6c9e145
amaroelias <amaronascimento2016@hotmail.com>
2026-03-10 15:34:44 -0300

    iniciando projeto spring boot

---
+++ src/test/java/com/gerenciador_backlog_api/GerenciadorBacklogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador_backlog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorBacklogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
